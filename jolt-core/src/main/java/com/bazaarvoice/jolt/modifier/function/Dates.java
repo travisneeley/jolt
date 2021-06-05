c37d817a8ced793c7e2869efc8673cb8d0409e4d
Travis Neeley <travis.neeley@gmail.com>
2021-06-05 09:23:12 -0400

    Pulled from Zambotn, not working

--- jolt-core/src/main/java/com/bazaarvoice/jolt/modifier/function/Dates.java
+++ jolt-core/src/main/java/com/bazaarvoice/jolt/modifier/function/Dates.java
@@ -16,129 +16,127 @@
 package com.bazaarvoice.jolt.modifier.function;
 
 import com.bazaarvoice.jolt.common.Optional;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import com.bazaarvoice.jolt.exception.TransformException;
+
 import java.time.Instant;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
  * @author Alessio Zamboni <alessio.zamboni@unitn.it>
  * @date: 25/11/2020
  */
-@SuppressWarnings( "deprecated" )
+@SuppressWarnings("deprecated")
 public class Dates {
-  public static Optional<Long> castToLong(Object obj) {
-    if(obj instanceof Number) {
-      return Optional.of(((Number) obj).longValue());
-    }
-    return Optional.empty();
-  }
-
-  /**
-   * Given a {@link java.lang.Number} representing an EPOCH in milliseconds and the pattern in which
-   * it has to be converted returns a String following that representation.
-   *
-   * @param arg EPOCH to be converted.
-   * @param format pattern in which the EPOCH has to be converted; it uses
-   * {@link java.text.SimpleDateFormat} format.
-   * @return String representing the date according to the <b>current locale/timezone</b>, wrapped
-   * in an {@link Optional} object.
-   * @see java.text.SimpleDateFormat
-   */
-  public static Optional<String> fromEpochMilli(Object arg, Object format) {
-    Optional<Long> optEpoch = castToLong(arg);
-    if ( arg == null || !(format instanceof String) || !optEpoch.isPresent()) {
-      return Optional.empty();
-    }
-
-    Long epoch = optEpoch.get();
-    String formatStr = (String) format;
-    Date d = Date.from(Instant.ofEpochMilli(epoch));
-    SimpleDateFormat dateFormat = new SimpleDateFormat(formatStr);
-    return Optional.<String>of(dateFormat.format(d));
-  }
-
-  /**
-   * Given a String representing a {@link java.util.Date} and the pattern used to represent it,
-   * returns the EPOCH in milliseconds of that date. The pattern uses same pattern in
-   * {@link java.text.SimpleDateFormat}.
-   * If not specify otherwise inside the pattern the date refers to the
-   * <b>current locale/timezone</b>.
-   *
-   * @param date String representing the date
-   * @param format String representing the pattern
-   * @return Long representing the EPOCH in the <b>current locale/timezone</b>, wrapped in
-   * {@link Optional}.
-   * @see java.text.SimpleDateFormat
-   */
-  public static Optional<Long> toEpochMilli(Object date, Object format) {
-    if ( !((date instanceof String) && (format instanceof String)) ) {
-      return Optional.empty();
-    }
-    String formatStr = (String) format;
-    String dateStr = (String) date;
-    SimpleDateFormat dateFormat = new SimpleDateFormat(formatStr);
-
-    try {
-      Date d = dateFormat.parse(dateStr);
-      return Optional.<Long>of( d.toInstant().toEpochMilli() );
-    } catch (ParseException e) {
-      e.printStackTrace();
-    }
-    return Optional.empty();
-  }
-
-  /**
-   * This function returns current time, in EPOCH, of the <b>current locale/timezone</b>.
-   * @return Long representing current EPOCH, wrapped by {@link Optional}
-   * @see Instant#now()
-   */
-  public static Optional<Long> now() {
-    return Optional.<Long>of(Instant.now().toEpochMilli());
-  }
-
-  public static final class now implements Function {
-    @Override
-    @SuppressWarnings( "unchecked" )
-    public Optional<Object> apply(Object... args) {
-      return (Optional) now();
-    }
-  }
-
-  @SuppressWarnings( "unchecked" )
-  public static final class fromEpochMilli extends Function.BaseFunction<Object> {
-
-    @Override
-    protected Optional<Object> applyList(List<Object> input) {
-      if ((input == null) && (input.size() != 2)) {
+    public static Optional<Long> castToLong(Object obj) {
+        if (obj instanceof Number) {
+            return Optional.of(((Number) obj).longValue());
+        }
         return Optional.empty();
-      } else {
-        return (Optional) fromEpochMilli(input.get(0), input.get(1));
-      }
     }
 
-    @Override
-    protected Optional<Object> applySingle(Object arg) {
-      return (Optional) fromEpochMilli(arg, "yyyyMMdd");
-    }
-  }
+    /**
+     * Given a {@link java.lang.Number} representing an EPOCH in milliseconds and the pattern in which
+     * it has to be converted returns a String following that representation.
+     *
+     * @param arg    EPOCH to be converted.
+     * @param format pattern in which the EPOCH has to be converted; it uses
+     *               {@link java.text.SimpleDateFormat} format.
+     * @return String representing the date according to the <b>current locale/timezone</b>, wrapped
+     * in an {@link Optional} object.
+     * @see java.text.SimpleDateFormat
+     */
+    public static Optional<String> fromEpochMilli(Object arg, Object format) {
+        Optional<Long> optEpoch = castToLong(arg);
+        if (arg == null || !(format instanceof String) || !optEpoch.isPresent()) {
+            return Optional.empty();
+        }
 
-  @SuppressWarnings( "unchecked" )
-  public static final class toEpochMilli extends Function.BaseFunction<Object> {
-
-    @Override
-    protected Optional<Object> applyList(List<Object> input) {
-      if ((input == null) || (input.size() != 2)) {
-        return Optional.empty();
-      } else {
-        return (Optional) toEpochMilli(input.get(0), input.get(1));
-      }
+        Long epoch = optEpoch.get();
+        String formatStr = (String) format;
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(formatStr);
+        return Optional.of(dateFormatter.format(Instant.ofEpochMilli(epoch)));
     }
 
-    @Override
-    protected Optional<Object> applySingle(Object arg) {
-      return Optional.empty();
+    /**
+     * Given a String representing a {@link java.util.Date} and the pattern used to represent it,
+     * returns the EPOCH in milliseconds of that date. The pattern uses same pattern in
+     * {@link java.text.SimpleDateFormat}.
+     * If not specify otherwise inside the pattern the date refers to the
+     * <b>current locale/timezone</b>.
+     *
+     * @param date   String representing the date
+     * @param format String representing the pattern
+     * @return Long representing the EPOCH in the <b>current locale/timezone</b>, wrapped in
+     * {@link Optional}.
+     * @see java.text.SimpleDateFormat
+     */
+    public static Optional<Long> toEpochMilli(Object date, Object format) {
+        if (!((date instanceof String) && (format instanceof String))) {
+            return Optional.empty();
+        }
+        String formatStr = (String) format;
+        String dateStr = (String) date;
+
+        try {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(formatStr);
+            return Optional.of(Instant.from(dateFormatter.parse(dateStr)).toEpochMilli());
+        } catch (Exception ex) {
+            throw new TransformException("Unable to format Date string into viable epoch", ex);
+        }
     }
-  }
+
+    /**
+     * This function returns current time, in EPOCH, of the <b>current locale/timezone</b>.
+     *
+     * @return Long representing current EPOCH, wrapped by {@link Optional}
+     * @see Instant#now()
+     */
+    public static Optional<Long> now() {
+        return Optional.of(Instant.now().toEpochMilli());
+    }
+
+    public static final class now implements Function {
+        @Override
+        @SuppressWarnings("unchecked")
+        public Optional<Object> apply(Object... args) {
+            return (Optional) now();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static final class fromEpochMilli extends Function.BaseFunction<Object> {
+
+        @Override
+        protected Optional<Object> applyList(List<Object> input) {
+            if ((input == null) && (input.size() != 2)) {
+                return Optional.empty();
+            } else {
+                return (Optional) fromEpochMilli(input.get(0), input.get(1));
+            }
+        }
+
+        @Override
+        protected Optional<Object> applySingle(Object arg) {
+            return (Optional) fromEpochMilli(arg, "yyyyMMdd");
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static final class toEpochMilli extends Function.BaseFunction<Object> {
+
+        @Override
+        protected Optional<Object> applyList(List<Object> input) {
+            if ((input == null) || (input.size() != 2)) {
+                return Optional.empty();
+            } else {
+                return (Optional) toEpochMilli(input.get(0), input.get(1));
+            }
+        }
+
+        @Override
+        protected Optional<Object> applySingle(Object arg) {
+            return Optional.empty();
+        }
+    }
 }
